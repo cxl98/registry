7359897efacefe9c811f5727f1a3c1e2df0df4e4
cxl <1799359457@qq.com>
2021-04-14 18:38:08 +0800

    update

--- registry-admin/src/main/java/com/cxl/registry/admin/controller/RegistryController.java
+++ registry-admin/src/main/java/com/cxl/registry/admin/controller/RegistryController.java
@@ -2,10 +2,9 @@ package com.cxl.registry.admin.controller;
 
 import com.cxl.registry.admin.core.model.Registry;
 import com.cxl.registry.admin.core.result.ReturnT;
-import com.cxl.registry.admin.service.IRegistryService;
+import com.cxl.registry.admin.service.impl.RegistryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +16,7 @@ import java.util.Map;
 public class RegistryController {
 
     @Autowired
-    private IRegistryService iRegistryService;
+    private RegistryService registryService;
 
     @RequestMapping("")
     public String index(){return "registry/registry.index";}
@@ -26,24 +25,24 @@ public class RegistryController {
     @ResponseBody
     public Map<String,Object> pageList(@RequestParam(required = false,defaultValue = "0")int start,
                                        @RequestParam(required =false,defaultValue = "0") int length,String biz,String env,String key){
-        return iRegistryService.pageList(start,length,biz,env,key);
+        return registryService.pageList(start,length,biz,env,key);
     }
     @RequestMapping("/delete")
     @ResponseBody
     public ReturnT<String> delete(int id){
-        return iRegistryService.delete(id);
+        return registryService.delete(id);
     }
 
     @RequestMapping("update")
     @ResponseBody
     public ReturnT<String> update(Registry registry){
-        return iRegistryService.update(registry);
+        return registryService.update(registry);
     }
 
     @RequestMapping("/add")
     @ResponseBody
     public ReturnT<String> add(Registry registry){
-        return iRegistryService.add(registry);
+        return registryService.add(registry);
     }
 
 }
